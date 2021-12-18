6683aefb7efb4f869047280b8c495f2187073e9c
aimecitar <maimesaracho@gmail.com>
2021-12-18 10:44:21 -0300

    añado spinner con condición lógica (operador ternario)

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
 import Frase from "./components/Frase";
 import { useState, useEffect } from "react";
+import Spinner from "./components/Spinner";
 
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [carga, setCarga] = useState(true);
+
   useEffect(() => {
     consultaAPI();
   }, []);
 
   const consultaAPI = async () => {
+    setCarga(true);
     const respuesta = await fetch(
       "https://thesimpsonsquoteapi.glitch.me/quotes"
     );
@@ -20,7 +24,16 @@ function App() {
     console.log(dato[0]);
     // guardar los datos dentro del state
     setPersonaje(dato[0]);
+    setCarga(false);
   };
+
+  // operador ternario
+  const mostrarComponente =
+    carga === true ? (
+      <Spinner></Spinner>
+    ) : (
+      <Frase personaje={personaje}></Frase>
+    );
 
   return (
     <section className="container my-5 d-flex flex-column align-items-center">
@@ -32,7 +45,7 @@ function App() {
       >
         Obtener frase
       </Button>
-      <Frase personaje={personaje}></Frase>
+      {mostrarComponente}
     </section>
   );
 }
